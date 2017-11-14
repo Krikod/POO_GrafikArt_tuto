208d761624227acc953d031af396795bfda34eee
Krikod <kristen.kod@gmail.com>
2017-11-14 18:06:13 +0100

    formulaire en POO

--- index.php
+++ index.php
@@ -1,25 +1,56 @@
 <?php
 
-require 'Personnage.php';
+require 'Form.php';
+
+$form = new Form($_POST);
+
+?>
+
+<form action="#" method="post">
+    <?php
+    echo $form->input('username');
+    echo $form->input('password');
+    echo $form->submit();
+    ?>
+</form>
 
 
 
-$merlin = new Personnage("Merlin");
-$harry = new Personnage("Harry");
-
-$merlin->setNom('Jules');
-
-var_dump($merlin->getNom());
-var_dump($merlin->getAtk());
 
 
-//$merlin->attaque($harry);
-//
-//if ($harry->mort()){
-//    echo 'Harry est mort !';
-//} else {
-//    echo  'Harry a survécu avec ' . $harry->vie . ' points de vie.';
-//}
-//
-//echo "<br>";
-//echo $merlin->getNom();
+
+
+
+
+
+
+
+
+
+
+
+
+<!--//require 'Personnage.php';-->
+<!---->
+<!---->
+<!--//$merlin = new Personnage("Merlin");-->
+<!--//$harry = new Personnage("Harry");-->
+<!--//-->
+<!--//$merlin->setNom('Jules');-->
+<!--//-->
+<!--//var_dump($merlin->getNom());-->
+<!--//var_dump($merlin->getAtk());-->
+<!---->
+<!---->
+<!---->
+<!---->
+<!--//$merlin->attaque($harry);-->
+<!--//-->
+<!--//if ($harry->mort()){-->
+<!--//    echo 'Harry est mort !';-->
+<!--//} else {-->
+<!--//    echo  'Harry a survécu avec ' . $harry->vie . ' points de vie.';-->
+<!--//}-->
+<!--//-->
+<!--//echo "<br>";-->
+<!--//echo $merlin->getNom();-->
