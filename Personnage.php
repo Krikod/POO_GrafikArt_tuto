0fcd564767ef7fcd528f8d62d5ae942bf1102e50
Krikod <kristen.kod@gmail.com>
2017-11-14 17:06:10 +0100

    visibilité var/func & getters/setters

--- Personnage.php
+++ Personnage.php
@@ -1,12 +1,14 @@
 <?php
 
 class Personnage {
+//Par défaut les variables sont en "private" ==> on crée des "getters"
+// pour y avoir accès en dehors de la classe.
+    private $vie = 40;
+    private $atk = 30;
+    private $nom;
+//"protected": moins strict que "private"
+// car on pourrait y accéder par des enfants de la classe (héritage).
 
-    public $vie = 40;
-    public $atk = 20;
-    public $nom;
-
-    
     public function __construct($nom){
         $this->nom = $nom;
     }
@@ -23,8 +25,38 @@ class Personnage {
         return $this->vie <= 0;
         }
 
+        //private car pas besoin en dehors de la classe (-> que dans la fonction attaque() ).
+    private function empecher_negatif() {
+        if ($this->vie <= 0){
+            $this->vie = 0;
+        }
+    }
+
     // Passer un objet en paramètre d'une fonction
     public function attaque($cible){
         $cible->vie -= $this->atk;
+        $cible->empecher_negatif();
     }
+
+    // SETTERS
+    public function setNom($nom){
+        $this->nom = $nom;
+    }
+
+
+    // GETTERS
+    public function getNom(){
+        return $this->nom;
+    }
+
+    public function getVie(){
+        return $this->vie;
+    }
+
+    public function getAtk(){
+        return $this->atk;
+    }
+
+
+
 }
--- index.php
+++ index.php
@@ -7,12 +7,19 @@ require 'Personnage.php';
 $merlin = new Personnage("Merlin");
 $harry = new Personnage("Harry");
 
+$merlin->setNom('Jules');
 
-$merlin->attaque($harry);
+var_dump($merlin->getNom());
+var_dump($merlin->getAtk());
 
-if ($harry->mort()){
-    echo 'Harry est mort !';
-} else {
-    echo  'Harry a survécu avec ' . $harry->vie . ' points de vie.';
-}
 
+//$merlin->attaque($harry);
+//
+//if ($harry->mort()){
+//    echo 'Harry est mort !';
+//} else {
+//    echo  'Harry a survécu avec ' . $harry->vie . ' points de vie.';
+//}
+//
+//echo "<br>";
+//echo $merlin->getNom();
